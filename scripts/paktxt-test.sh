f8f982161cc3e3839a4047153f39df1875bbc295
user <user@git.local>
2025-07-03 15:50:40 -0400

    Updating test script

--- scripts/paktxt-test.sh
+++ scripts/paktxt-test.sh
@@ -3,7 +3,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR/.."
 
 go install; \
-    rm -Rf build; \
-    mkdir build; \
-    ~/go/bin/paktxt pack -e README.md -o build.txt; \
-    ~/go/bin/paktxt unpack -w build -i build.txt
+    rm -Rf paktxt-test; \
+    mkdir paktxt-test; \
+    ~/go/bin/paktxt pack -e README.md -o paktxt-test.txt; \
+    ~/go/bin/paktxt unpack -w paktxt-test -i paktxt-test.txt
